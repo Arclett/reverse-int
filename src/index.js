3714fcaec7cce10358bcccb95a2b80cfc907070e
Aleksei Stoyanenko <stoynalex@gmail.com>
2022-06-26 19:38:38 +0500

    feat: task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    const h = Math.abs(n);
+    let revNum = h.toString().charAt((h.toString().length))
+    for (i = 0; i < h.toString().length+1; i++) {
+        revNum = revNum + h.toString().charAt((h.toString().length-i))
+    }
+    return Number(revNum);
 }
